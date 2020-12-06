c99eae105128b64160b10278361bbf94477e7b03
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-07 00:17:56 +0800

    Azure koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 try { 
-    $pdo_conn = new PDO('mysql:host=hmti.azurewebsites.net;dbname=pemilu', 'root', 'Pemiluhmti2021',
+    $pdo_conn = new PDO('mysql:host=MYSQLCONNSTR_localdb;dbname=localdb', 'root', '',
     array(PDO::ATTR_PERSISTENT => true)); 
 } 
 catch(PDOException $e) { echo $e->getMessage(); 
