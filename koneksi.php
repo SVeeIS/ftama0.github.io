2e6336e4d0c2045113b0ba494a424ed0e668269c
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-07 02:02:30 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
 $port=$_SERVER['WEBSITE_MYSQL_PORT'];
-$server	= "localhost:$port";
+$server="localhost:$port";
 $user="azure";
 $pass="6#vWHD_$";
 $db="localdb";
-$koneksidb = mysqli_connect( $server, $user, $pass, $db);
-if (! $koneksidb) {
-  echo "Failed Connection !";
+$koneksidb=mysqli_connect( $server, $user, $pass, $db);
+if (! $koneksidb){
+echo "Failed Connection !";
 }
 ?>
