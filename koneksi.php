f85884bb7244a00e0dceb4bbda371e4cc5e423a9
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-07 01:10:16 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,11 @@
 <?php 
-try { 
-    $pdo_conn = new PDO('mysql:host=localhost;dbname=localdb', 'root', '',
-    array(PDO::ATTR_PERSISTENT => true)); 
-} 
-catch(PDOException $e) { echo $e->getMessage(); 
-} 
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DBNAME", "pemilu"); /* misalnya nama database saya db_azurems */
+
+$conn = mysql_connect(HOSTNAME, USERNAME, PASSWORD)
+  or die ("Error saat menghubungkan ke host database");
+$db = mysql_select_db(DBNAME)
+  or die ("Error saat menghubungkan ke database");
 ?>
