8946909387553daf1628fc7623ea92e160d06bed
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-06 21:56:47 +0800

    kembali ke yang lalu

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,8 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:pemilu.database.windows.net,1433; Database = db_pemilu", "ferry", "Pemiluhmti2021");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ferry", "pwd" => "{your_password_here}", "Database" => "db_pemilu", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:pemilu.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+<?php 
+try { 
+    $pdo_conn = new PDO('mysql:host=localhost;dbname=pemilu', 'root', '',
+    array(PDO::ATTR_PERSISTENT => true)); 
+} 
+catch(PDOException $e) { echo $e->getMessage(); 
+} 
 ?>
