81c4fbd6516a480daee00c672fd7ad98b16cd810
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-06 23:03:06 +0800

    9999

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 try { 
-    $pdo_conn = new PDO('mysql:host=https://hmti.azurewebsites.net;dbname=pemilu', 'root', 'Pemiluhmti2021',
+    $pdo_conn = new PDO('mysql:host=hmti.azurewebsites.net;dbname=pemilu', 'root', 'Pemiluhmti2021',
     array(PDO::ATTR_PERSISTENT => true)); 
 } 
 catch(PDOException $e) { echo $e->getMessage(); 
