a37d729f7d62a4641b321e80c86b8ed5985d7482
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-07 01:31:25 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
-<?php 
-try { 
-    $pdo_conn = new PDO('mysql:host=localhost;dbname=localdb', 'azure', '6#vWHD_$',
-    array(PDO::ATTR_PERSISTENT => true)); 
-} 
-catch(PDOException $e) { echo $e->getMessage(); 
-} 
+<?php
+$myHost	= "localhost";
+$myUser	= "azure";
+$myPass	= "6#vWHD_$";
+$myDbs	= "localdb";
+
+$koneksidb = mysqli_connect( $myHost, $myUser, $myPass, $myDbs);
+if (! $koneksidb) {
+  echo "Failed Connection !";
+}
+
 ?>
