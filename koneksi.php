8c32c7c239201effc0a2902e2d5671ca3db7e513
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-07 01:49:25 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -5,21 +5,21 @@ $user	= "azure";
 $password	= "6#vWHD_$";
 $db	= "localdb";
 
-$mysqli = new mysqli("$server","$user","$password","$db");
+$pdo_conn = new mysqli("$server","$user","$password","$db");
 
 // Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+if ($pdo_conn -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $pdo_conn -> connect_error;
   exit();
 }
 
 // Perform query
-if ($result = $mysqli -> query("SELECT no_urut FROM vote")) {
+if ($result = $pdo_conn -> query("SELECT no_urut FROM vote")) {
   $row = $result->fetch_row();
   echo "<h1>$row[0]</h1>";
   // Free result set
   $result -> free_result();
 }
 
-$mysqli -> close();
+$pdo_conn -> close();
 ?>
