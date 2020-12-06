28f1511dc0191688a700a59f15743c7e2a250626
Ferry pratama <68576999+ftama0@users.noreply.github.com>
2020-12-07 00:36:43 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 try { 
-    $pdo_conn = new PDO('mysql:host=localhost;dbname=MYSQLCONNSTR_localdb', 'azure', 'Pemiluhmti2021',
+    $pdo_conn = new PDO('mysql:host=localhost;dbname=MYSQLCONNSTR_localdb', 'root', '',
     array(PDO::ATTR_PERSISTENT => true)); 
 } 
 catch(PDOException $e) { echo $e->getMessage(); 
