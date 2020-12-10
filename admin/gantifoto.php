8d71009743f6efa607a8f6a4565a172ec52c6ed8
SVeeIS <69779014+SVeeIS@users.noreply.github.com>
2020-12-10 11:23:02 +0800

    Create gantifoto.php

---
+++ admin/gantifoto.php
@@ -0,0 +1,145 @@
+<?php
+include('includes/config.php');
+
+if(isset($_POST['update'])){
+	$foto=$_FILES["foto"]["name"];
+	$id=$_POST['id'];
+	move_uploaded_file($_FILES["foto"]["tmp_name"],"../images/".$_FILES["foto"]["name"]);
+	$sql="update biodata_calon set foto='$foto' where nim_calon='$id'";
+	$query	= mysqli_query($koneksidb, $sql);
+	echo "<script type='text/javascript'>
+			alert('Berhasil ganti gambar.');
+			document.location = 'bioedit.php?id=$id'; 
+		</script>";
+}
+?>
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	
+	<link rel="icon" href="../Logo/HMTI2020.png">
+	<title>Pemilu HMTI | Admin Update Foto Calon</title>
+
+	<!-- Font awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Bootstrap Datatables -->
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<!-- Bootstrap social button library -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<!-- Bootstrap select -->
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<!-- Bootstrap file input -->
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<!-- Awesome Bootstrap checkbox -->
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<!-- Admin Stye -->
+	<link rel="stylesheet" href="css/style.css">
+<style>
+.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+</style>
+
+
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+	<div class="ts-main-content">
+	<?php include('includes/leftbar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+					
+						<h2 class="page-title">Foto Calon</h2>
+
+						<div class="row">
+							<div class="col-md-10">
+								<div class="panel panel-default">
+									<div class="panel-heading">Detail Foto Calon</div>
+									<div class="panel-body">
+										<form method="post" class="form-horizontal" enctype="multipart/form-data">
+
+											<div class="form-group">
+											<label class="col-sm-4 control-label">Foto Sekarang</label>
+												<?php 
+												$id=intval($_GET['nim_calon']);
+												$sql ="SELECT foto from biodata_calon where nim_calon='$id'";
+												$query	= mysqli_query($koneksidb, $sql);
+												$cnt=1;
+												while ($result = mysqli_fetch_array($query)){
+												?>
+												<div class="col-sm-8">
+													<img src="../images/<?php echo htmlentities($result['foto']);?>" width="300" height="200" style="border:solid 1px #000">
+												</div>
+											<?php }?>
+											</div>
+
+											<div class="form-group">
+											<input type="hidden" name="id" value="<?php echo $id; ?>"required>
+											<label class="col-sm-4 control-label">Upload Foto Baru<span style="color:red">*</span></label>
+												<div class="col-sm-8">
+													<input type="file" name="foto" required>
+												</div>
+											</div>
+											<div class="hr-dashed"></div>
+											
+											<div class="form-group">
+												<div class="col-sm-8 col-sm-offset-4">
+								
+													<button class="btn btn-primary" name="update" type="submit">Update</button>
+												</div>
+											</div>
+
+										</form>
+
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
